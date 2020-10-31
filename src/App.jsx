e7b31854d07304cc68c40156bb2ca6d03e63dca7
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-31 13:37:45 +0300

    travis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './app.scss';
 
 function App() {
-  return <div className="app">Total Calorie Calculator</div>;
+  return <div className="app">Total Calorie Calculator <div>hello</div></div>;
 }
 
 export default App;
